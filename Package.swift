4a150ad1d9e87dd163b45d0afc0f7e2014849824
Mykola <61082418+gulivero1773@users.noreply.github.com>
2026-08-19 16:53:35 +0300

    Update Package.swift
    Co-authored-by: Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.120.0"),
-        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.2.0", traits: []),
     ],
     targets: [
         .target(
