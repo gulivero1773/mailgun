c51bcd108240f7e380798c03d0e9177cffeedf75
Mykola Buhaiov <gulivero1773@icloud.com>
2026-08-19 14:57:10 +0300

    feat: update version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.117.0"),
-        .package(url: "https://github.com/apple/swift-configuration.git", from: "0.1.1"),
+        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.2.0"),
     ],
     targets: [
         .target(
